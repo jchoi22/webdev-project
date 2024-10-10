0ca1ff76e5f3c3d1596ec2daa94922a2e7937dc5
Lucy Ordway <lucyordway@Lucys-MacBook-Air-2.local>
2024-10-09 22:50:27 -0400

    Kickoff Lucy Attempt with attempt for parsing

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,7 @@
 import {useEffect, useState } from "react";
 import { getAllDesserts } from "../../Services/DessertList.js";
 import MainList from "./MainList.js";
-
+//sorry I am at Grace Hopper right now and very confused so I apologize, I know this is wrong
 const Main = () => {
   const [desserts, setDesserts] = useState([]);
 
@@ -10,7 +10,18 @@ const Main = () => {
   //     setDesserts(desserts);
   //   });
   // }, []);
+  useEffect(() => {
+    const parseDesserts = async () =>{
+        const Dessert = Parse.Object.extend('parse', 
+        {
+            getAllDesserts: function () {
+              return this.get('name') //idk I'm sorry
+            }
+        })
+    }
 
+  })
+  
   return (
     <div>
       <h1>Jaylen and Lucy's Bake Shop</h1>
