77aff949a228fd81995bc5460718ece554854680
Lucy Ordway <lordway@nd.edu>
2024-12-10 14:11:50 -0500

    fix order form

--- src/Components/Orders/CustOrders.js
+++ src/Components/Orders/CustOrders.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getOrdersByUserEmail } from "../../Services/UsersOrders"; 
+import "../../App.css";
 
 const UserOrders = () => {
   const [orders, setOrders] = useState([]);
@@ -19,7 +20,7 @@ const UserOrders = () => {
 //need to fix this part because it does not show the dessert title 
   return (
     <div>
-      <h1>Your Orders</h1>
+      <h2>Your Orders</h2>
       {orders.length === 0 ? (
         <p>You have no orders yet.</p>
       ) : (
@@ -27,11 +28,13 @@ const UserOrders = () => {
         {orders.map((order) => {
           const desserts = order.get("Desserts"); 
           return (
+            <div className="order-areas">
             <li key={order.id}>
               <p>Order ID: {order.id}</p>
-              <p>Order Details: {desserts?.title}</p> 
+              
               <p>Order Date: {order.get("createdAt").toLocaleDateString()}</p>
             </li>
+            </div>
           );
         })}
       </ul>
