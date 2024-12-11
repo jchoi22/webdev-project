4403abc776675cd72bf4b58c518a9e4b1b65ad2d
Jaylen Choi <jaylenchoi@Jaylens-MacBook-Air.local>
2024-12-10 22:34:20 -0500

    reorder auth

--- src/Components/Auth/AuthLogin.js
+++ src/Components/Auth/AuthLogin.js
@@ -64,7 +64,6 @@ const AuthLogin = () => {
   return (
     <div>
       
-      <Footer />
       
       <div>
       <p>Login here</p>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,37 +1,91 @@
-import { Link } from "react-router-dom";
-import { GiShoppingBag } from "react-icons/gi";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Parse from "parse";
 import { IoCart } from "react-icons/io5";
-import Cart from "../Cart/Cart"
-import { useContext } from "react";
 import { CartContext, useCartContext } from "../Cart/CartContext";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const { productsInCart, toggleCartVisibility } = useCartContext();
-  return (
-  <footer>
-    <nav>
-      <ul className="navigation">
-        <li> 
-          <Link to="/"> Home </Link>
-        </li>
-        <li>
-            <Link to = "/Orders"> Your Orders </Link>
-        </li>
-        <li>
-            <Link to = "/Desserts"> Desserts </Link>
-        </li>
-        <li>
-        <button className="icon-cart" onClick={toggleCartVisibility}>
-            <IoCart size={47} />
-            {productsInCart && productsInCart.length > 0 && (
-              <span className="product-count-bubble">{productsInCart.length}</span>
-            )}
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </footer>
-);
+  const user = Parse.User.current();
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+
+  // Handler for actions requiring login
+  const handleProtectedClickMyAccount = (action) => {
+    if (user?.authenticated) {
+      action();
+    } else {
+      setShowModal(true);
+    }
   };
+
+  const handleProtectedClickCart = (action) => {
+    if (user?.authenticated) {
+      action();
+    } else {
+      setShowModal(true);
+    }
+  };
+
+  const handleGoToLogin = () => {
+    navigate("/auth/login");
+    setShowModal(false);
+  };
+
+  const handleGoBack = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <footer>
+      <nav>
+        <ul className="navigation">
+          <li className="order-link">
+            <button
+              className="navigation-link"
+              onClick={() => handleProtectedClickMyAccount(() => navigate("/Orders"))}
+            >
+              My Account
+            </button>
+          </li>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/Desserts"> Desserts </Link>
+          </li>
+          <li>
+            <button
+              className="icon-cart"
+              onClick={() => handleProtectedClickCart(toggleCartVisibility)}
+            >
+              <IoCart size={47} />
+              {productsInCart && productsInCart.length > 0 && (
+                <span className="product-count-bubble">
+                  {productsInCart.length}
+                </span>
+              )}
+            </button>
+          </li>
+        </ul>
+      </nav>
+      
+      {/* Modal */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            
+            <p>You need to log in to access this page. Would you like to log in?</p>
+            <button onClick={handleGoToLogin}>Go to Login</button>
+            < br />
+            < br />
+            <button onClick={handleGoBack} className="close-btn">Go Back</button>
+          </div>
+        </div>
+      )}
+    </footer>
+  );
+};
 
 export default Footer;
